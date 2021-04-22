244714b005fbd15b1cf4dae237c272e61d078b40
corvinus-c24tc3 <Daviddadoroli2500>
2021-04-22 16:10:14 +0200

    9. hét

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -5,20 +5,34 @@ var válaszElem1;
 var válaszElem2;
 var válaszElem3;
 
-function letöltés() {
-    fetch("/questions.json")
-        .then(response => response.json())
-        .then(data => letöltésBefejeződött(data));
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(kérdésMegjelenítése);
 }
 
-function letöltésBefejeződött(d) {
-    console.log("Sikeres letöltés");
-    console.log(d);
-    kérdések = d;
+//function letöltés() {
+//    fetch("/questions.json")
+//        .then(response => response.json())
+//        .then(data => letöltésBefejeződött(data));
+//}
 
-    kérdésIndex = 0;
-    kérdésMegjelenítés(kérdések[kérdésIndex]);
-}
+//function letöltésBefejeződött(d) {
+//    console.log("Sikeres letöltés");
+//    console.log(d);
+//    kérdések = d;
+
+//    kérdésIndex = 0;
+//    kérdésMegjelenítés(kérdések[kérdésIndex]);
+//}
 
 function kérdésMegjelenítés(kérdés) {
 
@@ -29,78 +43,94 @@ function kérdésMegjelenítés(kérdés) {
     válaszElem2.innerHTML = kérdés.answer2;
     válaszElem3.innerHTML = kérdés.answer3;
 
-    var kép = document.getElementById("kép1");
-    kép.src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
-
-}
-
-function vissza() {
-    válaszVisszaállítása();
-    if (kérdésIndex == 0) {
-        kérdésIndex = kérdések.length-1;
-    } else {
-        kérdésIndex--;
+    if (!kérdés.image == "") {
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
     }
-    kérdésMegjelenítés(kérdések[kérdésIndex]);
-}
-
-function előre() {
-    válaszVisszaállítása();
-    if (kérdésIndex == kérdések.length-1) {
-        kérdésIndex = 0;
-    } else {
-        kérdésIndex++;
+    else {
+        document.getElementById("kép1").src = ""
     }
-    kérdésMegjelenítés(kérdések[kérdésIndex]);
+
 }
 
-function válasz1Katt() {
-    válaszKatt("válasz1", 1);
-}
+//function vissza() {
+//    válaszVisszaállítása();
+//    if (kérdésIndex == 0) {
+//        kérdésIndex = kérdések.length-1;
+//    } else {
+//        kérdésIndex--;
+//    }
+//    kérdésMegjelenítés(kérdések[kérdésIndex]);
+//}
 
-function válasz2Katt() {
-    válaszKatt("válasz2", 2);
-}
+//function előre() {
+//    válaszVisszaállítása();
+//    if (kérdésIndex == kérdések.length-1) {
+//        kérdésIndex = 0;
+//    } else {
+//        kérdésIndex++;
+//    }
+//    kérdésMegjelenítés(kérdések[kérdésIndex]);
+//}
 
-function válasz3Katt() {
-    válaszKatt("válasz3", 3);
-}
+//function válasz1Katt() {
+//    válaszKatt("válasz1", 1);
+//}
 
-function válaszKatt(válaszId, válaszIndex) {
-    var válaszElem = document.getElementById(válaszId);
-    if (kérdések[kérdésIndex].correctAnswer == válaszIndex) {
-        válaszElem.classList.add("jó");
-    } else {
-        válaszElem.classList.add("rossz");
-    }
-    válaszElem.classList.remove("kattintható");
-}
+//function válasz2Katt() {
+//    válaszKatt("válasz2", 2);
+//}
 
-function válaszVisszaállítása() {
-    válaszElem1.classList.remove("jó");
-    válaszElem1.classList.remove("rossz");
-    válaszElem1.classList.add("kattintható");
-    válaszElem2.classList.remove("jó");
-    válaszElem2.classList.remove("rossz");
-    válaszElem2.classList.add("kattintható");
-    válaszElem3.classList.remove("jó");
-    válaszElem3.classList.remove("rossz");
-    válaszElem3.classList.add("kattintható");
-}
+//function válasz3Katt() {
+//    válaszKatt("válasz3", 3);
+//}
 
-function clickEseményadás() {
-    válaszElem1 = document.getElementById("válasz1");
-    válaszElem2 = document.getElementById("válasz2");
-    válaszElem3 = document.getElementById("válasz3");
+//function válaszKatt(válaszId, válaszIndex) {
+//    var válaszElem = document.getElementById(válaszId);
+//    if (kérdések[kérdésIndex].correctAnswer == válaszIndex) {
+//        válaszElem.classList.add("jó");
+//    } else {
+//        válaszElem.classList.add("rossz");
+//    }
+//    válaszElem.classList.remove("kattintható");
+//}
 
-    document.getElementById("vissza").onclick = vissza;
-    document.getElementById("előre").onclick = előre;
-    válaszElem1.onclick = válasz1Katt;
-    válaszElem2.onclick = válasz2Katt;
-    válaszElem3.onclick = válasz3Katt;
-}
+//function válaszVisszaállítása() {
+//    válaszElem1.classList.remove("jó");
+//    válaszElem1.classList.remove("rossz");
+//    válaszElem1.classList.add("kattintható");
+//    válaszElem2.classList.remove("jó");
+//    válaszElem2.classList.remove("rossz");
+//    válaszElem2.classList.add("kattintható");
+//    válaszElem3.classList.remove("jó");
+//    válaszElem3.classList.remove("rossz");
+//    válaszElem3.classList.add("kattintható");
+//}
+
+//function clickEseményadás() {
+//    válaszElem1 = document.getElementById("válasz1");
+//    válaszElem2 = document.getElementById("válasz2");
+//    válaszElem3 = document.getElementById("válasz3");
+
+//    document.getElementById("vissza").onclick = vissza;
+//    document.getElementById("előre").onclick = előre;
+//    válaszElem1.onclick = válasz1Katt;
+//    válaszElem2.onclick = válasz2Katt;
+//    válaszElem3.onclick = válasz3Katt;
+//}
 
 window.onload = () => {
-    letöltés();
-    clickEseményadás();
+    //letöltés();
+
+    kérdésBetöltés(kérdésIndex);
+    //clickEseményadás();
+
+    document.getElementById("előre").onclick = () => {
+        sorszám++;
+        kérdésBetöltés(sorszám);
+    }
+
+    document.getElementById("vissza").onclick = () => {
+        sorszám--;
+        kérdésBetöltés(sorszám);
+    }
 }
